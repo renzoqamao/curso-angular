2f57b44203a53e388d5bbc5104c88fc37a70be0d
Renzo Quispe <renzoquispeamao@gmail.com>
2025-11-07 22:03:13 -0500

    @if

--- 02-bases/src/app/pages/dragonball/dragonball-page.component.ts
+++ 02-bases/src/app/pages/dragonball/dragonball-page.component.ts
@@ -16,7 +16,8 @@ export class DragonballPageComponent{
   characters = signal<Character[]>([
     {id: 1, name : "goku", power: 9001},
     {id: 2, name : "vegeta", power: 9000},
-    {id: 3, name : "picolo", power: 3000}]);
+    {id: 3, name : "picolo", power: 3000},
+    {id: 4, name : "yamcha", power: 500}]);
 
     /*powerClasses = computed(()=>{
       return {
