b6f6d0a8551db33c712632020c797f8c0dabf06c
Renzo Quispe <renzoquispeamao@gmail.com>
2025-11-08 10:59:13 -0500

    hashRouter

--- 02-bases/src/app/app.config.ts
+++ 02-bases/src/app/app.config.ts
@@ -2,11 +2,17 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    // HashStrategy: Añade un hash a la url permitiendo la navegación independiente del host:port
+    {
+      provide : LocationStrategy,
+      useClass: HashLocationStrategy
+    }
   ]
 };
