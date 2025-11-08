a499081941b1ce31e29ca691bd387fd94600314e
Renzo Quispe <renzoquispeamao@gmail.com>
2025-11-07 21:46:52 -0500

    ngClass

--- 02-bases/src/app/pages/dragonball/dragonball-page.component.ts
+++ 02-bases/src/app/pages/dragonball/dragonball-page.component.ts
@@ -1,4 +1,5 @@
-import { Component, signal } from "@angular/core";
+import { NgClass } from "@angular/common";
+import { Component, computed, signal } from "@angular/core";
 
 interface Character{
   id : number;
@@ -8,11 +9,18 @@ interface Character{
 
 @Component({
   templateUrl:  './dragonball-page.component.html',
+  //imports: [NgClass],
 })
 export class DragonballPageComponent{
 
   characters = signal<Character[]>([
     {id: 1, name : "goku", power: 9001},
-    {id: 2, name : "vegeta", power: 9002},
-    {id: 3, name : "picolo", power: 9003}])
+    {id: 2, name : "vegeta", power: 9000},
+    {id: 3, name : "picolo", power: 3000}]);
+
+    /*powerClasses = computed(()=>{
+      return {
+        'text-danger':true,
+      }
+    });*/
 }
