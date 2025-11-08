4c1f25d5d5f84233d52dc73ef5fe98aca31f5362
Renzo Quispe <renzoquispeamao@gmail.com>
2025-11-08 16:00:20 -0500

    routerLink desde componentes hijos

--- 03-gifs-app/src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
+++ 03-gifs-app/src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
+import { MenuOption } from '../../../interfaces/MenuOption';
+import { RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
   selector: 'gifs-side-menu-options',
   templateUrl: './side-menu-options.component.html',
+  imports: [RouterLink, RouterLinkActive],
 })
 
-export class SideMenuOptionsComponent{
+export class SideMenuOptionsComponent {
+  menuOptions: MenuOption[] = [{
+    icon: 'fa-solid fa-chart-line',
+    label: 'Trending',
+    subLabel: ' Gifs populares',
+    route: '/dashboard/trending'
+  },
+  {
+    icon: 'fa-solid fa-magnifying-glass',
+    label: 'Buscador',
+    subLabel: ' Buscar populares',
+    route: '/dashboard/search',
+  },
+  ]
 }
+
