032dc3892c88249825a8e63c4df7b42b62604343
Renzo Quispe <renzoquispeamao@gmail.com>
2025-11-08 10:51:54 -0500

    localstorage and effects

--- 02-bases/src/app/services/dragonball.service.ts
+++ 02-bases/src/app/services/dragonball.service.ts
@@ -1,11 +1,19 @@
-import { Injectable, signal } from '@angular/core';
+import { effect, Injectable, signal } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
+
+const loadFromLocalStorage = (): Character []=>{
+  const characters = localStorage.getItem('characters');
+  return characters ? JSON.parse(characters) : [];
+}
 
 @Injectable({providedIn: 'root'})
 export class DragonballService {
-  characters = signal<Character[]>([
-    {id: 1, name : "goku", power: 9001},
-    {id: 2, name : "vegeta", power: 9000}]);
+  characters = signal<Character[]>(loadFromLocalStorage());
+
+    saveToLocalStorage = effect(()=>{ //el efecto reacciona al cambio de la señal
+      console.log(`Character count ${this.characters().length}`);
+      localStorage.setItem('characters', JSON.stringify(this.characters()));
+    });
 
 
     addCharacter(character : Character){
