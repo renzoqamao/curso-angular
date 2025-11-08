aab4afff92a8be115c7741a01beb9661a1430992
Renzo Quispe <renzoquispeamao@gmail.com>
2025-11-07 21:36:43 -0500

    @for

--- 02-bases/src/app/pages/dragonball/dragonball-page.component.ts
+++ 02-bases/src/app/pages/dragonball/dragonball-page.component.ts
@@ -1,8 +1,18 @@
-import { Component } from "@angular/core";
+import { Component, signal } from "@angular/core";
+
+interface Character{
+  id : number;
+  name : string;
+  power : number;
+}
 
 @Component({
   templateUrl:  './dragonball-page.component.html',
 })
 export class DragonballPageComponent{
 
+  characters = signal<Character[]>([
+    {id: 1, name : "goku", power: 9001},
+    {id: 2, name : "vegeta", power: 9002},
+    {id: 3, name : "picolo", power: 9003}])
 }
