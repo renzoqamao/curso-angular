8cd741e4c0f531d13bac3367dae6e1afeeaf4f7f
Renzo Quispe <renzoquispeamao@gmail.com>
2025-11-09 18:06:23 -0500

    menu superiores e íconos

---
+++ 04-country-app/src/app/country/components/top-menu/top-menu.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'country-top-menu',
+  templateUrl: './top-menu.component.html'
+})
+
+export class TopMenuComponent {
+
+}
--- 04-country-app/src/app/country/layouts/country-layout/country-layout.component.ts
+++ 04-country-app/src/app/country/layouts/country-layout/country-layout.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TopMenuComponent } from "../../components/top-menu/top-menu.component";
 
 @Component({
   selector: 'country-layout',
-  imports : [RouterOutlet],
+  imports: [RouterOutlet, TopMenuComponent],
   templateUrl: './country-layout.component.html'
 })
 
