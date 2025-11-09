7bb4081a26b7493e343fabeb48931903b16855af
Renzo Quispe <renzoquispeamao@gmail.com>
2025-11-09 17:51:13 -0500

    footer conf

--- 04-country-app/src/app/app.ts
+++ 04-country-app/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomePageComponent } from "./shared/pages/home-page/home-page.component";
+import { FooterComponent } from "./shared/components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomePageComponent],
+  imports: [RouterOutlet, HomePageComponent, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- 04-country-app/src/app/shared/pages/home-page/home-page.component.ts
+++ 04-country-app/src/app/shared/pages/home-page/home-page.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'home-page',
+  imports : [RouterLink],
   templateUrl: './home-page.component.html'
 })
 
