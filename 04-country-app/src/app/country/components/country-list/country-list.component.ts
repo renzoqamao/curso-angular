34b4442e8c45f23f8cc85df17dec10de4c5c9290
Renzo Quispe <renzoquispeamao@gmail.com>
2025-11-13 18:41:51 -0500

    isLoading, error, results

--- 04-country-app/src/app/country/components/country-list/country-list.component.ts
+++ 04-country-app/src/app/country/components/country-list/country-list.component.ts
@@ -13,4 +13,7 @@ import { RouterLink } from "@angular/router";
 export class CountryListComponent {
 
   countries = input.required<Country[]>();
+  errorMessage = input<string|unknown|null>();
+  isLoading = input<boolean>(false);
+  isEmpty = input<boolean>(false);
 }
--- 04-country-app/src/app/country/services/country.service.ts
+++ 04-country-app/src/app/country/services/country.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { RestCountry } from '../interfaces/rest-country.interfaces';
-import { catchError, map, throwError } from 'rxjs';
+import { catchError, delay, map, throwError } from 'rxjs';
 import { CountryMapper } from '../../mappers/country.mapper';
 
 const API_URL = 'https://restcountries.com/v3.1';
@@ -29,6 +29,7 @@ export class CountryService {
      return this.http.get<RestCountry[]>(`${API_URL}/name/${query}`)
     .pipe(
       map(restCountries => CountryMapper.mapRestCountryArrayToCountryArray(restCountries) ),
+      delay(2000),
       catchError((error)=>{
         return throwError(()=> new Error(`No se pudo obtener países con ese query ${query}`));
       }
