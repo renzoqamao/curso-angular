84b3446279e0f187c33e3651ad68b595ac57567a
Renzo Quispe <renzoquispeamao@gmail.com>
2025-11-13 17:54:01 -0500

    reactividad con resources

--- 04-country-app/src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ 04-country-app/src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -1,10 +1,8 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, inject, resource, signal } from '@angular/core';
 import { SearchInputComponent } from "../../components/search-input/search-input.component";
 import { CountryListComponent } from "../../components/country-list/country-list.component";
 import { CountryService } from '../../services/country.service';
-import { RestCountry } from '../../interfaces/rest-country.interfaces';
-import { CountryMapper } from '../../../mappers/country.mapper';
-import { Country } from '../../interfaces/country.interface';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'by-capital-page',
@@ -15,26 +13,13 @@ import { Country } from '../../interfaces/country.interface';
 export class ByCapitalPageComponent  {
 
   countryService = inject(CountryService);
-  isLoading = signal(false);
-  isError = signal<string|null>(null);
-  countries = signal<Country[]>([]);
+  query = signal('');
+  countryResource = resource({
+    params:()=>({ query: this.query()}),
+    loader: async ({params})=>{
+      if(!params.query) return [];
+      return await firstValueFrom(this.countryService.searchByCapital(params.query));
+    }
+  });
 
-  onSearch(query : string){
-    if(this.isLoading()) return;
-    this.isLoading.set(true);
-    this.isError.set(null);
-    this.countryService
-    .searchByCapital(query)
-    .subscribe(
-      {next:(countries)=>{
-        this.isLoading.set(false);
-        this.countries.set(countries);
-      },
-      error: (err)=>{
-        this.isLoading.set(false);
-        this.countries.set([]);
-        this.isError.set(err);
-      }}
-    );
-  }
 }
