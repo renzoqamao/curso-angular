adfc8eb125c2138222ca030100e04a3a6f97b71e
Renzo Quispe <renzoquispeamao@gmail.com>
2025-11-12 23:48:41 -0500

    pipes

--- 04-country-app/src/app/country/components/country-list/country-list.component.ts
+++ 04-country-app/src/app/country/components/country-list/country-list.component.ts
@@ -1,10 +1,12 @@
 import { Component, input, OnInit } from '@angular/core';
 import { RestCountry } from '../../interfaces/rest-country.interfaces';
 import { Country } from '../../interfaces/country.interface';
+import { DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'country-list',
-  templateUrl: './country-list.component.html'
+  templateUrl: './country-list.component.html',
+  imports: [DecimalPipe]
 })
 
 export class CountryListComponent {
