2fd311597c05162f9be23122c27cffe110c3cb49
Renzo Quispe <renzoquispeamao@gmail.com>
2025-11-14 16:47:08 -0500

    actualización de input y query

--- 04-country-app/src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ 04-country-app/src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -4,7 +4,7 @@ import { CountryListComponent } from "../../components/country-list/country-list
 import { CountryService } from '../../services/country.service';
 import { of } from 'rxjs';
 import { rxResource } from '@angular/core/rxjs-interop'
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'by-capital-page',
   templateUrl: './by-capital-page.component.html',
@@ -15,13 +15,19 @@ export class ByCapitalPageComponent {
 
   countryService = inject(CountryService);
   activatedRoute = inject(ActivatedRoute);
+  router = inject(Router);
+
   queryParam = this.activatedRoute.snapshot.queryParamMap.get('query') ?? '';
   query = linkedSignal(()=>this.queryParam);
   countryResource = rxResource({
     params: this.query,
     stream: ({ params }) => {
-      console.log({query: params})
       if (!params) return of([])
+      this.router.navigate(['/country/by-capital'],{
+        queryParams:{
+          query: params
+        }
+    });
       return this.countryService.searchByCapital(params)
     }
   })
