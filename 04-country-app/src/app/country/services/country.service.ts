e039ba4bbfc1d37f3ced6955a8580bdd16871070
Renzo Quispe <renzoquispeamao@gmail.com>
2025-11-14 13:36:48 -0500

    cache con un Map

--- 04-country-app/src/app/country/services/country.service.ts
+++ 04-country-app/src/app/country/services/country.service.ts
@@ -1,8 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { RestCountry } from '../interfaces/rest-country.interfaces';
-import { catchError, delay, map, of, throwError } from 'rxjs';
+import { catchError, delay, map, of, tap, throwError } from 'rxjs';
 import { CountryMapper } from '../../mappers/country.mapper';
+import { Country } from '../interfaces/country.interface';
 
 const API_URL = 'https://restcountries.com/v3.1';
 
@@ -10,12 +11,15 @@ const API_URL = 'https://restcountries.com/v3.1';
 export class CountryService {
 
   private http = inject(HttpClient);
+  private queryCacheCapital = new Map<string, Country[]>();
 
   searchByCapital(query: string){
     query = query.toLowerCase();
+    if( this.queryCacheCapital.has(query)) return of(this.queryCacheCapital.get(query));
     return this.http.get<RestCountry[]>(`${API_URL}/capital/${query}`)
     .pipe(
       map(restCountries => CountryMapper.mapRestCountryArrayToCountryArray(restCountries) ),
+      tap((countries)=> this.queryCacheCapital.set(query,countries)),
       catchError((error)=>{
         return throwError(()=> new Error(`No se pudo obtener países con ese query ${query}`));
       }
