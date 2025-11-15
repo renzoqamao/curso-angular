f24aecc3c87537ac2315c5469e73a4c8fe35fe82
Renzo Quispe <renzoquispeamao@gmail.com>
2025-11-15 00:11:44 -0500

    custom pipes to sort table and transform cell

--- 05-pipes-app/src/app/pages/custom-page/custom-page.component.ts
+++ 05-pipes-app/src/app/pages/custom-page/custom-page.component.ts
@@ -1,14 +1,26 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { ToggleCasePipe } from '../../pipes/toggle-case.pipe';
-
+import { heroes } from '../../data/heroes.data';
+import { CanFyPipe } from '../../pipes/can-fly.pipe';
+import { HeroColorPipe } from '../../pipes/heroColor.pipe';
+import { TitleCasePipe } from '@angular/common';
+import { HeroTextColorPipe } from '../../pipes/hero-text-color.pipe';
+import { HeroCreatorPipe } from '../../pipes/hero-creator.pipe';
+import { Hero } from '../../interfaces/hero.interface';
+import { HeroFilterPipe } from '../../pipes/hero-filter.pipe';
+import { HeroSortByPipe } from '../../pipes/hero-sort-by.pipe';
 @Component({
   selector: 'app-custom-page.component',
-  imports: [ToggleCasePipe],
+  imports: [ToggleCasePipe, CanFyPipe, HeroColorPipe, TitleCasePipe, HeroTextColorPipe, HeroCreatorPipe,HeroFilterPipe,HeroSortByPipe],
   templateUrl: './custom-page.component.html',
 })
 export default class CustomPageComponent {
   name = signal('Renzo Quispe');
   upperCase = signal(true);
 
+  heroes = signal(heroes);
+  sortBy = signal<keyof Hero | null>(null);
+
+  searchQuery = signal('');
 
 }
