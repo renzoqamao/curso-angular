e07ab6e294b35dac9ab432467054d0c28de566a2
Renzo Quispe <renzoquispeamao@gmail.com>
2025-11-14 22:28:44 -0500

    i18nSelectPipe

--- 05-pipes-app/src/app/pages/uncommon-page/uncommon-page.component.ts
+++ 05-pipes-app/src/app/pages/uncommon-page/uncommon-page.component.ts
@@ -1,9 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CardComponent } from '../../components/card/card.component';
+import { I18nSelectPipe } from '@angular/common';
+
+const client1 = {
+  name: 'fernando',
+  gender: 'male',
+  age: 39,
+  address: 'Ottawa, Canadá'
+}
+
+const client2 = {
+  name: 'Melissa',
+  gender: 'female',
+  age: 33,
+  address: 'Toronto, Canadá'
+}
 
 @Component({
   selector: 'app-uncommon-page.component',
-  imports: [CardComponent],
+  imports: [CardComponent, I18nSelectPipe],
   templateUrl: './uncommon-page.component.html',
 })
-export default class UncommonPageComponent { }
+export default class UncommonPageComponent {
+
+  // i18n Select
+  client = signal(client1);
+  invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla'
+  }
+  changeClient() {
+    if (this.client() === client1) {
+      this.client.set(client2)
+      return;
+    }
+    this.client.set(client1)
+  }
+}
