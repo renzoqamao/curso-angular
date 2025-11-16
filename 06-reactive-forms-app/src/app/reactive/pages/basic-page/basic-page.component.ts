d7adee7310c479439b3b599d227318aaaac2e4d2
Renzo Quispe <renzoquispeamao@gmail.com>
2025-11-15 22:10:56 -0500

    validacion de campos incluidos

--- 06-reactive-forms-app/src/app/reactive/pages/basic-page/basic-page.component.ts
+++ 06-reactive-forms-app/src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,6 +1,6 @@
 import { JsonPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-page',
@@ -12,9 +12,15 @@ export class BasicPageComponent {
   fb = inject(FormBuilder);
 
   myForm = this.fb.group({
-    name: ['', /* validadadores sincronos*/, /* validadores asincronos*/   ],
-    price:[0],
-    inStorage:[0],
+    name: ['',
+       /* validadadores sincronos*/[Validators.required, Validators.minLength(3)],
+       /* validadores asincronos*/   ],
+    price:[0,
+      [Validators.required,Validators.min(10)]
+    ],
+    inStorage:[0,
+      [Validators.required,Validators.min(0)]
+    ],
   });
 
   /*myForm = new FormGroup({
