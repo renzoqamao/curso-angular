d2987d3832625b715b34d6d9cda317d3564a3e1a
Renzo Quispe <renzoquispeamao@gmail.com>
2025-11-20 17:18:15 -0500

    titulo en el navbar observable a señal con toSignal

--- 08-maps-app/src/app/shared/components/navbar/navbar.component.ts
+++ 08-maps-app/src/app/shared/components/navbar/navbar.component.ts
@@ -1,8 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { routes } from '../../../app.routes';
+import { NavigationEnd, Router, RouterLink } from '@angular/router';
+import { filter, map, tap } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [AsyncPipe, RouterLink],
   templateUrl: './navbar.component.html',
 })
-export class NavbarComponent { }
+export class NavbarComponent {
+
+  router = inject(Router);
+
+  routes = routes.map((route) =>({
+    path: route.path,
+    title: route.title ?? 'Maps en angular',
+  })).filter(route => route.path != '**' );
+
+  /*pageTitle$ = this.router.events.pipe(
+    filter(event => event instanceof NavigationEnd),
+    //tap(event => console.log(event)),
+    map(event  => event.url),
+    map(url => routes.find(route=> `/${route.path}`=== url)?.title ?? 'Mapas' )
+  )*/
+
+  pageTitle = toSignal(this.router.events.pipe(
+    filter(event => event instanceof NavigationEnd),
+    map(event  => event.url),
+    map(url => routes.find(route=> `/${route.path}`=== url)?.title ?? 'Mapas' )))
+
+  // pageTitle$ , el caracter '$' es porque es un observable
+  // para pasarlo como señal utilizamos el ToSignal para pasar de un observable a
+
+ }
