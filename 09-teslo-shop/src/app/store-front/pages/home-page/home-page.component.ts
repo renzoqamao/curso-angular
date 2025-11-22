5ea1b24b98385cb88547c08291a999f829b2ea9c
Renzo Quispe <renzoquispeamao@gmail.com>
2025-11-22 00:02:08 -0500

    traer listado de productos; productServices

--- 09-teslo-shop/src/app/store-front/pages/home-page/home-page.component.ts
+++ 09-teslo-shop/src/app/store-front/pages/home-page/home-page.component.ts
@@ -1,6 +1,8 @@
+import { rxResource } from '@angular/core/rxjs-interop';
 //import { ProductCardComponent } from '@/products/components/product-card/product-card.component';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ProductCardComponent } from '@products/components/product-card/product-card.component';
+import { ProductsService } from '@products/services/products.service';
 //import { ProductCardComponent } from '../../../products/components/product-card/product-card.component';
 
 @Component({
@@ -8,4 +10,14 @@ import { ProductCardComponent } from '@products/components/product-card/product-
   imports: [ProductCardComponent],
   templateUrl: './home-page.component.html',
 })
-export class HomePageComponent { }
+export class HomePageComponent {
+  productService = inject(ProductsService);
+
+  productsResource = rxResource({
+    params : ()=>({}),
+    stream:({params})=> {
+      return this.productService.getProducts();
+    }
+  })
+
+ }
