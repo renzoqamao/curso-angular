4f2cb02f364188b27cab86ec265ed6522ca74c1b
Renzo Quispe <renzoquispeamao@gmail.com>
2025-11-22 15:22:14 -0500

    pipe personalizado para las Url de las imagenes de los productos

--- 09-teslo-shop/src/app/products/components/product-card/product-card.component.ts
+++ 09-teslo-shop/src/app/products/components/product-card/product-card.component.ts
@@ -2,17 +2,18 @@ import { SlicePipe } from '@angular/common';
 import { Component, computed, input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Product } from '@products/interfaces/product.interface';
+import { ProductImagePipe } from '@products/pipes/product-image.pipe';
 
 @Component({
   selector: 'product-card',
-  imports: [RouterLink, SlicePipe],
+  imports: [RouterLink, SlicePipe, ProductImagePipe],
   templateUrl: './product-card.component.html',
 })
 export class ProductCardComponent {
   product = input.required<Product>();
 
-  imageUrl = computed(()=>{
-    return `http://localhost:3000/api/files/product/${this.product().images[0]}`
-  });
+  // imageUrl = computed(()=>{
+  //   return `http://localhost:3000/api/files/product/${this.product().images[0]}`
+  // });
 
  }
