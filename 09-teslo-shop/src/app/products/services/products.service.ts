582f435223096eaf033130d25fe4e932ec0f823d
Renzo Quispe <renzoquispeamao@gmail.com>
2025-11-23 15:18:17 -0500

    update caché

--- 09-teslo-shop/src/app/products/services/products.service.ts
+++ 09-teslo-shop/src/app/products/services/products.service.ts
@@ -58,6 +58,23 @@ export class ProductsService {
     productLike: Partial<Product>
   ): Observable<Product> {
     return this.http
-      .patch<Product>(`${baseUrl}/products/${id}`, productLike);
+      .patch<Product>(`${baseUrl}/products/${id}`, productLike)
+      .pipe(tap((product) => this.updateProductCache(product)));
+  }
+
+
+  updateProductCache(product: Product) {
+    const productId = `${product.id}`;
+
+    this.productCache.set(productId, product);
+
+    this.productsCache.forEach((productResponse) => {
+      productResponse.products = productResponse.products.map(
+        (currentProduct) =>
+          currentProduct.id === productId ? product : currentProduct
+      );
+    });
+
+    console.log('Caché actualizado');
   }
 }
