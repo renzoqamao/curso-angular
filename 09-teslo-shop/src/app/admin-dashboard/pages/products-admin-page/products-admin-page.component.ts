04e38b389735b0fd7577a2c02a4e6d58f0721156
Renzo Quispe <renzoquispeamao@gmail.com>
2025-11-23 11:49:11 -0500

    listado de productos admin con páginación

--- 09-teslo-shop/src/app/admin-dashboard/pages/products-admin-page/products-admin-page.component.ts
+++ 09-teslo-shop/src/app/admin-dashboard/pages/products-admin-page/products-admin-page.component.ts
@@ -1,8 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { ProductsService } from '@products/services/products.service';
+import { RouterLink } from '@angular/router';
+import { ProductTableComponent } from '@products/components/product-table/product-table.component';
+import { PaginationComponent } from '@/sharedcomponent/pagination/pagination.component';
+import { PaginationService } from '@/sharedcomponent/pagination/pagination.service';
 
 @Component({
-  selector: 'app-products-admin-page.component',
-  imports: [],
+  selector: 'app-products-admin-page',
+  imports: [ProductTableComponent, PaginationComponent, RouterLink],
   templateUrl: './products-admin-page.component.html',
 })
-export class ProductsAdminPageComponent { }
+export class ProductsAdminPageComponent {
+  productsService = inject(ProductsService);
+  paginationService = inject(PaginationService);
+
+  productsPerPage = signal(10);
+
+  productsResource = rxResource({
+    params: () => ({
+      page: this.paginationService.currentPage() - 1,
+      limit: this.productsPerPage(),
+    }),
+    stream: ({ params }) => {
+      return this.productsService.getProducts({
+        offset: params.page * 9,
+        limit: params.limit,
+      });
+    },
+  });
+}
